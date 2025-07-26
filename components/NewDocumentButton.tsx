b8fd12f6ebf0c46c49c043bc178b8d36feb667d4
Jithendra Varma <152156017+Jithendra1101@users.noreply.github.com>
2025-07-26 07:47:04 +0530

    Fix errors in creating new documents

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -39,5 +39,3 @@ function NewDocumentButton() {
 }
 
 export default NewDocumentButton;
-
-
--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,18 +1,23 @@
 import { initializeApp,App,getApps,cert } from "firebase-admin/app";
-
 import { getFirestore } from "firebase-admin/firestore";
 
+const serviceKey = process.env.FIREBASE_ADMIN_SDK_CREDENTIALS 
+    ? JSON.parse(process.env.FIREBASE_ADMIN_SDK_CREDENTIALS)
+    : null;
 
-const serviceKey = JSON.parse(process.env.FIREBASE_ADMIN_SDK_CREDENTIALS || '{}');
+if (!serviceKey) {
+    throw new Error("FIREBASE_ADMIN_SDK_CREDENTIALS environment variable is not set");
+}
+
 let app: App;
 
-if(getApps().length === 0) {
-app = initializeApp({
-    credential: cert(serviceKey),
+if (getApps().length === 0) {
+    app = initializeApp({
+        credential: cert(serviceKey),
     });
 } else {
     app = getApps()[0];
 }
 
 const adminDb = getFirestore(app);
-export { app as adminapp,adminDb };
+export { app as adminApp, adminDb };
